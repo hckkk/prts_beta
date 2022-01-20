d05097ff4125863b6150d40d69e7d31a2530324e
hckkk <2659871636@qq.com>
2022-01-19 16:46:39 -0800

    Initial index.js

--- commands/ping.js
+++ commands/ping.js
@@ -15,7 +15,7 @@ module.exports = {
         const user = interaction.user;
         if (user&&(integer>=0)){
             await interaction.reply('收到！');
-            await wait((135-integer)*5*60);
+            await wait((135-integer)*5*60*1000);
             //await wait(2000);
             await client.channels.cache.get('933472514028494899').send(`清理智啦！${user}`);
         }else {
