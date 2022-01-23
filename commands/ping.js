a6be47813a1b2a34b9b275a1dfca3094b0f458a7
hckkk <2659871636@qq.com>
2022-01-23 00:19:16 -0800

    fix

--- commands/ping.js
+++ commands/ping.js
@@ -7,13 +7,19 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('sans')
         .setDescription('理智满了会提醒你哦!')
-        .addIntegerOption(option => option.setName('理智剩余').setDescription('理智剩余')),
+        .addIntegerOption(option => option.setName('理智剩余').setDescription('理智剩余'))
+        .addIntegerOption(option => option.setName('理智上限').setDescription('理智上限')),
 
         //.addUserOption(option => option.setName('目标').setDescription('目标（选自己）')),
     async execute(interaction,client) {
         const integer = interaction.options.getInteger('理智剩余');
+        let up = interaction.options.getInteger('理智上限');
+        if(!up){
+            up=135;
+        }
+
         const user = interaction.user;
-        if (user&&(integer>=0)&&(integer<=135)){
+        if (user&&(integer>=0)&&(integer<=up)&&(up>0)&&(up<=135)){
             if (users[user]){
                 await interaction.reply('收到，倒计时已更新！');
             }else {
@@ -21,7 +27,7 @@ module.exports = {
             }
             let x = Date()
             users[user]=x;
-            await wait((135-integer)*6*60*1000);
+            await wait((up-integer)*6*60*1000);
             if (users[user]===x){
                 await client.channels.cache.get('931761629127921734').send(`清理智啦！${user}`);
                 users.delete(user);
